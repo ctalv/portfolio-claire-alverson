6cf60df729e69cd0410730e097e3ff66285f8004
Claire Alverson <clairetalverson@gmail.com>
2024-06-06 20:50:56 -0500

    Added base scroll effects

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,19 @@
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+window.addEventListener('scroll', function() {
+        
+    let scrollDistance = (window.scrollY/this.window.innerHeight)*100;
+    console.log(scrollDistance)
+    console.log('scrollll')
+
+    // once user hits a certain point add passion stuff 
+
+    // what you love about biology
+
+    // what you love about coding
+
+    // some passion projects
+
+    // where I am now
+
+    // how to contact me and linkedin
+    
+});
